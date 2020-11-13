c50e74233cad9689eaf33e1a08e16c18b393a0f5
Bauaser <michal.bala.1989@gmail.com>
2020-11-13 20:01:39 +0100

    Board creation started

--- src/main/java/com/kodilla/battleshipGame/Game/Battleship.java
+++ src/main/java/com/kodilla/battleshipGame/Game/Battleship.java
@@ -11,7 +11,6 @@ public class Battleship {
             this.size = size;
         }
 
-
         public int getSize() {
             return size;
         }
---
+++ src/main/java/com/kodilla/battleshipGame/Game/Board.java
@@ -0,0 +1,37 @@
+package com.kodilla.battleshipGame.Game;
+
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.control.Cell;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import java.awt.*;
+
+public class Board extends Parent {
+    private final int rowsNumber = 10;
+    private final int columnsNumber = 10;
+
+    private int shipCount;
+    private boolean playerBoard;
+    private VBox rows = new VBox();
+
+
+    public Board(EventHandler<? super MouseEvent> handler, boolean playerBoard) {
+        this.playerBoard = playerBoard;
+        for (int i = 0; i < rowsNumber; i++) {
+            HBox row = new HBox();
+            for (int j = 0; j < columnsNumber; j++) {
+                Cell cell = new Cell();
+                cell.setOnMouseClicked(handler);
+                row.getChildren().add(cell);
+            }
+            rows.getChildren().add(row);
+        }
+        getChildren().add(rows);
+    }
+
+    public Cell getCell(int x, int y) {
+        return (Cell)((HBox)rows.getChildren().get(y)).getChildren().get(x);
+    }
+}
