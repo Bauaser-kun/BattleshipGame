b1ffca8ff6645581538fb06cf610332c17c91b07
Bauaser <michal.bala.1989@gmail.com>
2020-11-13 21:32:04 +0100

    nested class Cell added to Board class

--- src/main/java/com/kodilla/battleshipGame/Game/Board.java
+++ src/main/java/com/kodilla/battleshipGame/Game/Board.java
@@ -6,7 +6,8 @@ import javafx.scene.control.Cell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import java.awt.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Board extends Parent {
     private final int rowsNumber = 10;
@@ -22,7 +23,7 @@ public class Board extends Parent {
         for (int y = 0; y < rowsNumber; y++) {
             HBox row = new HBox();
             for (int x = 0; x < columnsNumber; x++) {
-                Cell cell = new Cell();
+                Cell cell = new Cell(x, y, this);
                 cell.setOnMouseClicked(handler);
                 row.getChildren().add(cell);
             }
@@ -33,5 +34,48 @@ public class Board extends Parent {
 
     public Cell getCell(int x, int y) {
         return (Cell)((HBox)rows.getChildren().get(y)).getChildren().get(x);
+    }
+
+    public class Cell extends Rectangle {
+        public int x;
+        public int y;
+        public Battleship ship;
+        private boolean wasAimed;
+        private Board board;
+        private boolean isEmpty;
+
+        public Cell(int x, int y, Board board) {
+            super(20, 20);
+            this.x = x;
+            this.y = y;
+            this.board = board;
+            setFill(Color.AQUAMARINE);
+            setStroke(Color.GRAY);
+        }
+
+        public boolean isWasAimed() {
+            return wasAimed;
+        }
+
+        public boolean isEmpty() {
+            return isEmpty;
+        }
+
+        public boolean shoot() {
+            wasAimed = true;
+            setFill(Color.BLACK);
+
+            if (ship != null ) {
+                ship.wasShoot();
+                setFill(Color.RED);
+
+                if (!ship.isNotSinked()) {
+                    board.shipCount--;
+                }
+                return true;
+            }
+            return false;
+        }
+
     }
 }
